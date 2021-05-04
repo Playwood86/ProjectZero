3f5f5f661315e9fee2a8559fd5fdb55cf3ab61bb
R2-D2 <R2-D2@R2-D2.local>
2021-05-04 14:04:31 +0200

    moved

--- wp-content/themes/roysocialbrothers/footer.php
+++ wp-content/themes/roysocialbrothers/footer.php
@@ -3,8 +3,7 @@
 </div><!-- /.container -->
 
 <footer class="blog-footer">
-    <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
-    <p><a href="#">Back to top</a></p>
+    <p>&copy; Copyright <a href="https://socialbrothers.nl">Social Brothers</a> - <?php echo date("Y"); ?></p>
 </footer>
 <?php wp_footer(); ?>
 </body>
--- wp-content/themes/roysocialbrothers/functions.php
+++ wp-content/themes/roysocialbrothers/functions.php
@@ -1,14 +1,14 @@
 <?php
 
 function bootstrapstarter_enqueue_styles() {
-    wp_register_style('bootstrap', get_template_directory_uri() . '/bootstrap/css/bootstrap.min.css' );
+    wp_register_style('bootstrap', get_template_directory_uri() . '/vendor/bootstrap/css/bootstrap.min.css' );
     $dependencies = array('bootstrap');
 	wp_enqueue_style( 'bootstrapstarter-style', get_stylesheet_uri(), $dependencies ); 
 }
 
 function bootstrapstarter_enqueue_scripts() {
     $dependencies = array('jquery');
-    wp_enqueue_script('bootstrap', get_template_directory_uri().'/bootstrap/js/bootstrap.min.js', $dependencies, '', true );
+    wp_enqueue_script('bootstrap', get_template_directory_uri().'/vendor/bootstrap/js/bootstrap.min.js', $dependencies, '', true );
 }
 
 add_action( 'wp_enqueue_scripts', 'bootstrapstarter_enqueue_styles' );
